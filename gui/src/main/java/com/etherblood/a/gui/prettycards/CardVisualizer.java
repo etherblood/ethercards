0fd5e17edda01f8cb2a5bbc95044a4d9fcf6362b
carlkuesters <carl.kuesters@youengineering.com>
2020-04-05 03:55:49 +0200

    Update cardgui version, using the new 3d card model and removing dead code, which is now inside library.

---
+++ gui/src/main/java/com/etherblood/a/gui/prettycards/CardVisualizer.java
@@ -0,0 +1,15 @@
+package com.etherblood.a.gui.prettycards;
+
+import com.destrostudios.cardgui.BoardObjectModel;
+import com.destrostudios.cardgui.Card;
+import com.destrostudios.cardgui.samples.visualization.CustomAttachmentVisualizer;
+import com.jme3.asset.AssetManager;
+import com.jme3.scene.Node;
+
+public abstract class CardVisualizer<CardModelType extends BoardObjectModel> extends CustomAttachmentVisualizer<Card<CardModelType>, Node, CardVisualization> {
+
+    @Override
+    protected CardVisualization createVisualizationObject(AssetManager assetManager) {
+        return new CardVisualization(assetManager);
+    }
+}
