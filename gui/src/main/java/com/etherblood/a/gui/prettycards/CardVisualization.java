96a42d9314d314c2a51306492ee036849d3ce2ab
etherblood <philipp@jobach.de>
2020-06-11 06:00:43 +0200

    updated cards-gui version

--- gui/src/main/java/com/etherblood/a/gui/prettycards/CardVisualization.java
+++ gui/src/main/java/com/etherblood/a/gui/prettycards/CardVisualization.java
@@ -2,8 +2,8 @@ package com.etherblood.a.gui.prettycards;
 
 import com.destrostudios.cardgui.samples.visualization.CustomAttachmentVisualization;
 import com.destrostudios.cardgui.samples.visualization.GlowBox;
-import com.destrostudios.cardgui.samples.visualization.ModelledCard;
 import com.destrostudios.cardgui.samples.visualization.PaintableImage;
+import com.destrostudios.cardgui.samples.visualization.cards.modelled.LightingModelledCard;
 import com.jme3.asset.AssetManager;
 import com.jme3.math.ColorRGBA;
 import com.jme3.scene.Node;
@@ -12,12 +12,12 @@ public class CardVisualization extends CustomAttachmentVisualization<Node> {
 
     public CardVisualization(AssetManager assetManager) {
         node = new Node();
-        modelledCard = new ModelledCard(assetManager, "models/card/card.j3o", "images/cardbacks/magic.png", ColorRGBA.Black);
+        modelledCard = new LightingModelledCard(assetManager, "models/card/card.j3o", "images/cardbacks/magic.png", ColorRGBA.Black);
         node.attachChild(modelledCard.getNode());
         glowBox = new GlowBox(assetManager, 0.96f, 1.28f);
     }
     private Node node;
-    private ModelledCard modelledCard;
+    private LightingModelledCard modelledCard;
     private GlowBox glowBox;
 
     public void setCardFront(PaintableImage paintableImage) {
