bd40e57f18c71be20f89c54bc0ac2c09bdff53b5
etherblood <philipp@jobach.de>
2020-06-28 02:58:42 +0200

    Bee Warrior nerf

--- rules/src/main/java/com/etherblood/a/rules/EntityUtil.java
+++ rules/src/main/java/com/etherblood/a/rules/EntityUtil.java
@@ -9,7 +9,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class EntityUtil {
