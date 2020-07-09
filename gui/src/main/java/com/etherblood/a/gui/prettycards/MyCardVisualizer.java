c50d90e01fbf2328c8dfc5fd42d07512516ce793
etherblood <philipp@jobach.de>
2020-07-09 02:33:35 +0200

    Added temporary? cards-cache for better subsequent loading times

--- gui/src/main/java/com/etherblood/a/gui/prettycards/MyCardVisualizer.java
+++ gui/src/main/java/com/etherblood/a/gui/prettycards/MyCardVisualizer.java
@@ -3,9 +3,14 @@ package com.etherblood.a.gui.prettycards;
 import com.destrostudios.cardgui.Card;
 import com.destrostudios.cardgui.samples.visualization.PaintableImage;
 import com.jme3.asset.AssetManager;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MyCardVisualizer extends CardVisualizer<CardModel> {
 
+    // this cache assumes card images are static which works atm...
+    // TODO: replace this with better optimizations which don't assume staticness
+    private static final Map<String, PaintableImage> FULL_CARD_CACHE = new ConcurrentHashMap<>();
     private final CardPainterJME cardPainter;
 
     public MyCardVisualizer(CardPainterJME cardPainter) {
@@ -14,24 +19,32 @@ public class MyCardVisualizer extends CardVisualizer<CardModel> {
 
     @Override
     protected void updateVisualizationObject(CardVisualization visualization, Card<CardModel> card, AssetManager assetManager) {
-        PaintableImage back = cardPainter.createPaintable();
-        PaintableImage art = cardPainter.createPaintable();
-        PaintableImage front = cardPainter.createPaintable();
-        if (card.getModel().getZone() == BoardZone.BATTLE) {
-            if (card.getModel().isInspected()) {
-                cardPainter.drawMinion_Full(card.getModel(), back, art, front);
+        if (!card.getModel().isFoil() && card.getModel().getZone() != BoardZone.BATTLE && FULL_CARD_CACHE.containsKey(card.getModel().getTemplate().getAlias())) {
+            PaintableImage image = FULL_CARD_CACHE.get(card.getModel().getTemplate().getAlias());
+            visualization.setCardFront(image);
+            image.flipY();
+        } else {
+            PaintableImage back = cardPainter.createPaintable();
+            PaintableImage art = cardPainter.createPaintable();
+            PaintableImage front = cardPainter.createPaintable();
+            if (card.getModel().getZone() == BoardZone.BATTLE) {
+                if (card.getModel().isInspected()) {
+                    cardPainter.drawMinion_Full(card.getModel(), back, art, front);
+                } else {
+                    cardPainter.drawMinion_Minified(card.getModel(), back, art, front);
+                }
             } else {
-                cardPainter.drawMinion_Minified(card.getModel(), back, art, front);
+                cardPainter.drawCard(card.getModel(), back, art, front);
             }
-        } else {
-            cardPainter.drawCard(card.getModel(), back, art, front);
-        }
-        if (card.getModel().isFoil()) {
-            visualization.setFront(back, art, front);
-        } else {
-            back.paintImage(art, 0, 0, back.getWidth(), back.getHeight());
-            back.paintImage(front, 0, 0, back.getWidth(), back.getHeight());
-            visualization.setCardFront(back);
+            if (card.getModel().isFoil()) {
+                visualization.setFront(back, art, front);
+            } else {
+                back.paintImage(art, 0, 0, back.getWidth(), back.getHeight());
+                back.paintImage(front, 0, 0, back.getWidth(), back.getHeight());
+                visualization.setCardFront(back);
+                FULL_CARD_CACHE.put(card.getModel().getTemplate().getAlias(), back);
+                back.flipY();
+            }
         }
 
         if (card.getModel().getGlow() != null) {
