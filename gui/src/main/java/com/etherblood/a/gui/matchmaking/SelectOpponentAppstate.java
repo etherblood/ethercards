7740ebde2da8269defd90a877b1edeeba3733f3c
etherblood <philipp@jobach.de>
2020-07-19 16:00:24 +0200

    added sliders to select amount of humans/bots

--- gui/src/main/java/com/etherblood/a/gui/matchmaking/SelectOpponentAppstate.java
+++ gui/src/main/java/com/etherblood/a/gui/matchmaking/SelectOpponentAppstate.java
@@ -7,16 +7,19 @@ import com.jme3.asset.AssetManager;
 import com.jme3.scene.Node;
 import com.simsilica.lemur.Button;
 import com.simsilica.lemur.Container;
+import com.simsilica.lemur.DefaultRangedValueModel;
+import com.simsilica.lemur.Slider;
 
 public class SelectOpponentAppstate extends AbstractAppState {
 
-    private final int[] teamHumanCounts = {1, 0};
     private int teamSize = 1;
+    private final int strength = 10_000;
 
     private final Node guiNode;
     private final Container container;
     private final Button toggleTeamSizeButton;
     private final Button confirmButton;
+    private final Slider[] humanCountSliders;
 
     private boolean finalized = false;
 
@@ -26,27 +29,36 @@ public class SelectOpponentAppstate extends AbstractAppState {
         container = new Container();
         toggleTeamSizeButton = new Button("");
         toggleTeamSizeButton.addClickCommands(x -> toggleTeamSize());
-        toggleTeamSizeButton.setLocalTranslation(100, 200, 0);
         container.addChild(toggleTeamSizeButton);
+        container.setLocalTranslation(0, 800, 0);
+        int teamCount = 2;
+        humanCountSliders = new Slider[teamCount];
+        for (int i = 0; i < humanCountSliders.length; i++) {
+            int humans = i == 0 ? 1 : 0;
+            Slider slider = new Slider(new DefaultRangedValueModel(0, teamSize, humans) {
+
+                @Override
+                public void setValue(double value) {
+                    super.setValue(Math.round(value));
+                    updateGuiElements();
+                }
+
+            });
+            container.addChild(slider);
+            humanCountSliders[i] = slider;
+        }
         confirmButton = new Button("confirm");
         confirmButton.addClickCommands(x -> finalized = true);
-        confirmButton.setLocalTranslation(100, 300, 0);
         container.addChild(confirmButton);
-        container.setLocalTranslation(0, 900, 0);
     }
 
     private void toggleTeamSize() {
         teamSize ^= 3;
-        for (int i = 0; i < teamHumanCounts.length; i++) {
-            if (teamHumanCounts[i] > teamSize) {
-                teamHumanCounts[i] = teamSize;
-            }
-        }
-        updateButtons();
+        updateGuiElements();
     }
 
-    private void updateButtons() {
-        int teamCount = teamHumanCounts.length;
+    private void updateGuiElements() {
+        int teamCount = humanCountSliders.length;
         StringBuilder builder = new StringBuilder();
         builder.append(teamSize);
         for (int i = 1; i < teamCount; i++) {
@@ -54,11 +66,19 @@ public class SelectOpponentAppstate extends AbstractAppState {
             builder.append(teamSize);
         }
         toggleTeamSizeButton.setText(builder.toString());
+        for (Slider slider : humanCountSliders) {
+            slider.getModel().setMaximum(teamSize);
+            slider.getThumbButton().setText(Long.toString(Math.round(slider.getModel().getValue())));
+        }
     }
 
     public MatchOpponents getMatchOpponents() {
         if (finalized) {
-            return new MatchOpponents(teamHumanCounts, teamSize, 10_000);
+            int[] teamHumanCounts = new int[humanCountSliders.length];
+            for (int i = 0; i < teamHumanCounts.length; i++) {
+                teamHumanCounts[i] = Math.toIntExact(Math.round(humanCountSliders[i].getModel().getValue()));
+            }
+            return new MatchOpponents(teamHumanCounts, teamSize, strength);
         }
         return null;
     }
@@ -68,7 +88,7 @@ public class SelectOpponentAppstate extends AbstractAppState {
         finalized = false;
         stateManager.getState(HudTextAppstate.class).setText("Select your opponent.");
         guiNode.attachChild(container);
-        updateButtons();
+        updateGuiElements();
     }
 
     @Override
