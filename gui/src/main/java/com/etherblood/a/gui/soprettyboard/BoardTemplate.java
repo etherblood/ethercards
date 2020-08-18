f6b51f366a02401e02940afa177db56770d77ae9
etherblood <philipp@jobach.de>
2020-08-18 23:22:52 +0200

    players own zones always displayed on bottom left

--- gui/src/main/java/com/etherblood/a/gui/soprettyboard/BoardTemplate.java
+++ gui/src/main/java/com/etherblood/a/gui/soprettyboard/BoardTemplate.java
@@ -9,6 +9,7 @@ import com.jme3.math.Vector3f;
 import com.jme3.math.Vector4f;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class BoardTemplate {
 
@@ -23,22 +24,30 @@ public class BoardTemplate {
         Quaternion rotation = new Quaternion();
 
         IntList teamPlayers = teamToPlayer.get(team);
-        createTeamPlayerZones(teamPlayers, teamZoneSize, rotation, cardSize);
+        createTeamPlayerZones(teamPlayers, self, teamZoneSize, rotation, cardSize);
 
         for (IntList otherTeamPlayers : teamToPlayer.values()) {
             if (teamPlayers == otherTeamPlayers) {
                 continue;
             }
             rotation = rotation.mult(new Quaternion().fromAngles(0, FastMath.TWO_PI / teamToPlayer.size(), 0));
-            createTeamPlayerZones(otherTeamPlayers, teamZoneSize, rotation, cardSize);
+            createTeamPlayerZones(otherTeamPlayers, null, teamZoneSize, rotation, cardSize);
         }
     }
 
-    private void createTeamPlayerZones(IntList teamPlayers, Vector2f teamZoneSize, Quaternion rotation, Vector2f cardSize) {
+    private void createTeamPlayerZones(IntList teamPlayers, Integer self, Vector2f teamZoneSize, Quaternion rotation, Vector2f cardSize) {
         Vector2f playerZoneSize = new Vector2f(teamZoneSize.x / teamPlayers.size(), teamZoneSize.y);
         PlayerZonesTemplate playerZonesTemplate = new PlayerZonesTemplate(new Vector4f(0, 0, playerZoneSize.x, playerZoneSize.y), cardSize);
         Vector3f position = new Vector3f(-teamPlayers.size() * playerZoneSize.x / 2, 0, 0);
+        if(self != null) {
+            PlayerZones playerZone = playerZonesTemplate.create(rotation.mult(position), rotation);
+            playerZones.put(self, playerZone);
+            position.addLocal(playerZoneSize.x, 0, 0);
+        }
         for (int player : teamPlayers) {
+            if(Objects.equals(self, player)) {
+                continue;
+            }
             PlayerZones playerZone = playerZonesTemplate.create(rotation.mult(position), rotation);
             playerZones.put(player, playerZone);
             position.addLocal(playerZoneSize.x, 0, 0);
