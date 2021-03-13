c52cc8df89824a29a6191d7adb2a841d7cb0d7c3
etherblood <philipp@jobach.de>
2021-03-13 20:47:57 +0100

    removed chgrp

--- releng/deploy.sh
+++ releng/deploy.sh
@@ -7,7 +7,6 @@ CLIENT=$3
 cp ${GIT}gui/target/gui-*[!s].jar ${CLIENT}a.jar
 rsync -rc --delete ${GIT}gui/target/assets ${CLIENT}
 rsync -rc --delete ${GIT}gui/target/libs ${CLIENT}
-chgrp -R etherblood-tools ${CLIENT}
 curl https://destrostudios.com:8080/apps/5/updateFiles
 echo updated client files
 
@@ -17,7 +16,6 @@ echo stopped server
 cp ${GIT}game-server/target/game-server-*[!s].jar ${SERVER}a-cards.jar
 rsync -rc --delete ${GIT}game-server/target/libs ${SERVER}
 rsync -rc --delete ${GIT}game-server/target/assets ${SERVER}
-chgrp -R etherblood-tools ${SERVER}
 echo updated server files
 bash start.sh
 echo started server
