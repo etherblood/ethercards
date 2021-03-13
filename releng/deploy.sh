e3247d180f57cb96bf98d06730cb29a287bf3c2b
etherblood <philipp@jobach.de>
2021-03-13 20:35:14 +0100

    releng fixes

--- releng/deploy.sh
+++ releng/deploy.sh
@@ -7,16 +7,16 @@ CLIENT=$3
 cp ${GIT}gui/target/gui-*[!s].jar ${CLIENT}a.jar
 rsync -rc --delete ${GIT}gui/target/assets ${CLIENT}
 rsync -rc --delete ${GIT}gui/target/libs ${CLIENT}
-chown -R etherblood:etherblood-tools ${CLIENT}
+chgrp -R etherblood-tools ${CLIENT}
 curl https://destrostudios.com:8080/apps/5/updateFiles
-echo updated client
+echo updated client files
 
-files cd ${SERVER} sh stop.sh
+cd ${SERVER} bash stop.sh
 echo stopped server
 cp ${GIT}game-server/target/game-server-*[!s].jar ${SERVER}a-cards.jar
 rsync -rc --delete ${GIT}game-server/target/libs ${SERVER}
 rsync -rc --delete ${GIT}game-server/target/assets ${SERVER}
-chown -R etherblood:etherblood-tools ${SERVER}
+chgrp -R etherblood-tools ${SERVER}
 echo updated server files
-sh start.sh
+bash start.sh
 echo started server
