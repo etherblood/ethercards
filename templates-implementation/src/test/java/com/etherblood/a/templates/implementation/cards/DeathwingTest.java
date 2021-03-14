3a18664a08592eb891fe806a676f6aa0599092e2
etherblood <philipp@jobach.de>
2021-03-14 12:16:50 +0100

    nerfed aggro a lot

--- templates-implementation/src/test/java/com/etherblood/a/templates/implementation/cards/DeathwingTest.java
+++ templates-implementation/src/test/java/com/etherblood/a/templates/implementation/cards/DeathwingTest.java
@@ -1,11 +1,10 @@
 package com.etherblood.a.templates.implementation.cards;
 
 import com.etherblood.a.entities.collections.IntList;
-import com.etherblood.a.rules.DeathOptions;
 import com.etherblood.a.rules.moves.Cast;
-import com.etherblood.a.rules.moves.Update;
 import com.etherblood.a.templates.implementation.AbstractGameTest;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class DeathwingTest extends AbstractGameTest {
@@ -26,8 +25,20 @@ public class DeathwingTest extends AbstractGameTest {
         Assertions.assertTrue(data.has(minion1, core.IN_GRAVEYARD_ZONE));
         Assertions.assertTrue(data.has(card0, core.IN_GRAVEYARD_ZONE));
         Assertions.assertTrue(data.has(card1, core.IN_HAND_ZONE));
-        
+
         Assertions.assertTrue(data.has(hero(0), core.IN_BATTLE_ZONE));
         Assertions.assertTrue(data.has(hero(1), core.IN_BATTLE_ZONE));
+    }
+
+    @Disabled("intended behaviour which is currently being implemented")
+    @Test
+    public void does_not_trigger_lathliss() {
+        data.set(player(0), core.MANA, Integer.MAX_VALUE);
+        createMinion(player(0), "lathliss_dragon_queen");
+        int deathwing = createCard(player(0), "deathwing", core.IN_HAND_ZONE);
+
+        moves.apply(new Cast(player(0), deathwing));
+
+        Assertions.assertEquals(new IntList(hero(0), hero(1), deathwing), data.list(core.IN_BATTLE_ZONE));
     }
 }
