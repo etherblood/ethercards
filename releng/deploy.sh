230bf3341a67eb5ca9e23b16f019bdbd9238cc85
Etherblood <etherblood@users.noreply.github.com>
2021-10-10 18:42:17 +0200

    Update deploy.sh

--- releng/deploy.sh
+++ releng/deploy.sh
@@ -4,18 +4,18 @@ GIT=$1
 SERVER=$2
 CLIENT=$3
 
-cp ${GIT}gui/target/gui-*[!s].jar ${CLIENT}a.jar
-rsync -rc --delete ${GIT}gui/target/assets ${CLIENT}
-rsync -rc --delete ${GIT}gui/target/libs ${CLIENT}
+cp ${GIT}/gui/target/gui-*[!s].jar ${CLIENT}/a.jar
+rsync -rc --delete ${GIT}/gui/target/assets ${CLIENT}/
+rsync -rc --delete ${GIT}/gui/target/libs ${CLIENT}/
 curl https://destrostudios.com:8080/apps/5/updateFiles
 echo updated client files
 
 cd ${SERVER}
 bash stop.sh
 echo stopped server
-cp ${GIT}game-server/target/game-server-*[!s].jar ${SERVER}a-cards.jar
-rsync -rc --delete ${GIT}game-server/target/libs ${SERVER}
-rsync -rc --delete ${GIT}game-server/target/assets ${SERVER}
+cp ${GIT}/game-server/target/game-server-*[!s].jar ${SERVER}/a-cards.jar
+rsync -rc --delete ${GIT}/game-server/target/libs ${SERVER}/
+rsync -rc --delete ${GIT}/game-server/target/assets ${SERVER}/
 echo updated server files
 bash start.sh
 echo started server
